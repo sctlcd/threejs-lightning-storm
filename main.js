994c958614bfd734ce2d5314c2260e8a993e25b4
sctlcd <lucinda.souchet@gmail.com>
2023-02-10 01:09:42 +0100

    define scene and camera

--- main.js
+++ main.js
@@ -3,29 +3,34 @@ import * as THREE from 'three';
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+// const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+// camera.position.z = 1;
 
+// Define scene
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+// Define camera
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
+// const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+// const material = new THREE.MeshNormalMaterial();
 
-// animation
+// const mesh = new THREE.Mesh( geometry, material );
+// scene.add( mesh );
 
-function animation( time ) {
+// const renderer = new THREE.WebGLRenderer( { antialias: true } );
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.setAnimationLoop( animation );
+// document.body.appendChild( renderer.domElement );
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+// // animation
 
-	renderer.render( scene, camera );
+// function animation( time ) {
 
-}
+// 	mesh.rotation.x = time / 2000;
+// 	mesh.rotation.y = time / 1000;
+
+// 	renderer.render( scene, camera );
+
+// }
