6ee7fd3803bc4317c237b71cd6ae27e1ed173603
sctlcd <lucinda.souchet@gmail.com>
2023-02-17 01:58:51 +0100

    create cloud background with lightning flash

--- main.js
+++ main.js
@@ -3,30 +3,129 @@ import * as THREE from 'three';
 
 // Define scene
 const scene = new THREE.Scene();
-
 // Define camera: .PerspectiveCamera(field of view in degree, aspect ratio = width / height, near, far) 
 const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-// Add camera position
-camera.position.z = 1;
-// Add camera rotation
-camera.rotation.x = 1.16;
-camera.rotation.y = -0.12;
-camera.rotation.z = 0.27;
-
-// Define ambiant light: Illuminate all objects in the scene from all direction
-const ambiant = new THREE.AmbientLight( 0x555555 );
-// Define directional light: reresent the moon light in the sky
-const directionalLight = new THREE.DirectionalLight( 0xffeedd );
-// By default, the light will seems to come from above. To change he position light, I must move the whole light
-directionalLight.position.set( 0,0,1 );
-// Add ambiant light and directional light to scene
-scene.add( ambiant,directionalLight );
-
-// Define render
+// Define lightning flash: Setup a pointLight with blue color
+const flash = new THREE.PointLight(0x062d89, 30, 500 ,1.7);
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
-// Add fog to scene
-scene.fog = new THREE.FogExp2( 0x11111f, 0.002 );
-renderer.setClearColor( scene.fog.color );
-renderer.setSize( window.innerWidth, window.innerHeight );
-// Add renderer to HTML as a canvas element
-document.body.appendChild( renderer.domElement );
+const cloudParticles1 = [];
+const cloudParticles2 = [];
+
+function init() {
+  // Set camera position
+  camera.position.z = 1;
+  // Set the rotation angle of th ecamera to look up into the sky
+  camera.rotation.x = 1.16;
+  camera.rotation.y = -0.12;
+  camera.rotation.z = 0.27;
+
+  // Define ambiant light: Illuminate all objects in the scene from all direction
+  const ambiant = new THREE.AmbientLight( 0x555555 );
+  // Define directional light: reresent the moon light in the sky
+  const directionalLight = new THREE.DirectionalLight( 0xffeedd );
+  // By default, the light will seems to come from above. To change he position light, I must move the whole light
+  directionalLight.position.set( 0,0,1 );
+  // Add ambiant light and directional light into scene
+  scene.add( ambiant,directionalLight );
+
+  // Set lightning flash position behind the cloud
+  flash.position.set(200,300,100);
+  // Add lightning flash to the scene
+  scene.add(flash);
+  
+  // Add fog into scene
+  scene.fog = new THREE.FogExp2( 0x11111f, 0.002 );
+  renderer.setClearColor( scene.fog.color );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  // Add renderer into HTML as a canvas element
+  document.body.appendChild( renderer.domElement );
+
+  // Define Texture loader
+  const loader = new THREE.TextureLoader();
+  // Set Texture loader
+  const texture1 = loader.load("./images/vecteezy_rain-clouds-and-black-sky-textured-background_10123744_360.jpg");
+  const texture2 = loader.load("./images/vecteezy_rain-clouds-and-black-sky-textured-background_10121810_444.jpg");
+
+  // Cloud 1
+  // Define a geometry - 2000 unit plain square
+  const cloudGeometry1 = new THREE.PlaneBufferGeometry(2000,2000);
+  // Define a material and map it to the texture 1 
+  const cloudMaterial1 = new THREE.MeshLambertMaterial({
+    map: texture1,
+    transparent: true
+  });
+
+  // Loop to randomly add each cloud to the scene
+  for(let p=0; p<25; p++) {
+    const cloud1 = new THREE.Mesh(cloudGeometry1, cloudMaterial1);
+    cloud1.position.set(
+      Math.random()*800 -400,
+      500,
+      Math.random()*500 - 450
+    );
+    // Set the cloud rotation angle to face the camera
+    cloud1.rotation.x = 1.16;
+    cloud1.rotation.y = -0.12;
+    // Add some random around the z axis
+    cloud1.rotation.z = Math.random()*360;
+    cloud1.material.opacity = 0.8;
+
+    // Store the reference to each cloud in an array
+    cloudParticles1.push(cloud1);
+    
+    // Add Cloud1 into scene
+    scene.add(cloud1);
+  }
+
+  // Cloud 2
+  const cloudGeometry2 = new THREE.PlaneBufferGeometry(2000,2000);
+  const cloudMaterial2 = new THREE.MeshLambertMaterial({
+    map: texture2,
+    transparent: true
+  });
+
+  for(let p=0; p<25; p++) {
+    const cloud2 = new THREE.Mesh(cloudGeometry2, cloudMaterial2);
+    cloud2.position.set(
+      Math.random()*800 -400,
+      500,
+      Math.random()*500 - 450
+    );
+    cloud2.rotation.x = 1.16;
+    cloud2.rotation.y = -0.12;
+    cloud2.rotation.z = Math.random()*360;
+    cloud2.material.opacity = 0.6;
+
+    cloudParticles2.push(cloud2);
+    
+    scene.add(cloud2);
+  }
+
+  animate();
+};
+
+function animate() {
+  // In the array of clouds rotate the cloud one by one
+  cloudParticles1.forEach(p => {
+    p.rotation.z -=0.0004;
+  });
+
+  cloudParticles2.forEach(p => {
+    p.rotation.z -=0.0001;
+  });
+
+  // Random the flash position and light intensity 
+  if(Math.random() > 0.93 || flash.power > 100) {
+    if(flash.power < 100) 
+      flash.position.set(
+        Math.random()*400,
+        300 + Math.random() *200,
+        100
+      );
+    flash.power = 50 + Math.random() * 500;
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+init();
